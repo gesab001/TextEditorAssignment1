82cd8f179f0e9aa2b0314f4f402875cd0b57fbf1
Adam Petherick <a_petherick@hotmail.com>
2019-08-22 19:31:38 +1200

    Adding search function

--- src/texteditor/MainFrame.java
+++ src/texteditor/MainFrame.java
@@ -5,6 +5,13 @@
  */
 package texteditor;
 
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Document;
+import javax.swing.text.Highlighter;
+import javax.swing.text.JTextComponent;
+
 /**
  *
  * @author 14400
@@ -29,6 +36,9 @@ public class MainFrame extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jPanel1 = new javax.swing.JPanel();
+        searchBtn = new javax.swing.JButton();
+        searchField = new javax.swing.JTextField();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -37,15 +47,14 @@ public class MainFrame extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JPopupMenu.Separator();
         jMenuItem10 = new javax.swing.JMenuItem();
         jMenuItem11 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        Search = new javax.swing.JMenu();
+        exitApplication = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenuItem7 = new javax.swing.JMenuItem();
-        jMenuItem8 = new javax.swing.JMenuItem();
-        jMenuItem9 = new javax.swing.JMenuItem();
+        copyTxt = new javax.swing.JMenuItem();
+        pasteTxt = new javax.swing.JMenuItem();
+        cutTxt = new javax.swing.JMenuItem();
+        selectTxt = new javax.swing.JMenuItem();
+        timeandDate = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
         jMenuItem12 = new javax.swing.JMenuItem();
 
@@ -54,6 +63,31 @@ public class MainFrame extends javax.swing.JFrame {
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
+
+        searchBtn.setText("Search");
+        searchBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchBtnActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(searchField)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(searchBtn)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(searchBtn)
+                .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
 
         jMenu1.setText("File");
 
@@ -82,48 +116,45 @@ public class MainFrame extends javax.swing.JFrame {
         jMenuItem11.setText("Export to PDF");
         jMenu1.add(jMenuItem11);
 
-        jMenuItem4.setLabel("Exit");
-        jMenu1.add(jMenuItem4);
+        exitApplication.setLabel("Exit");
+        jMenu1.add(exitApplication);
 
         jMenuBar1.add(jMenu1);
-
-        Search.setLabel("Search");
-        jMenuBar1.add(Search);
 
         jMenu2.setLabel("View");
         jMenuBar1.add(jMenu2);
 
         jMenu3.setLabel("Manage");
 
-        jMenuItem5.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_MASK));
-        jMenuItem5.setText("Copy");
-        jMenu3.add(jMenuItem5);
+        copyTxt.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_MASK));
+        copyTxt.setText("Copy");
+        jMenu3.add(copyTxt);
 
-        jMenuItem6.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_V, java.awt.event.InputEvent.CTRL_MASK));
-        jMenuItem6.setLabel("Paste");
-        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
+        pasteTxt.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_V, java.awt.event.InputEvent.CTRL_MASK));
+        pasteTxt.setLabel("Paste");
+        pasteTxt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem6ActionPerformed(evt);
+                pasteTxtActionPerformed(evt);
             }
         });
-        jMenu3.add(jMenuItem6);
+        jMenu3.add(pasteTxt);
 
-        jMenuItem7.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_K, java.awt.event.InputEvent.CTRL_MASK));
-        jMenuItem7.setLabel("Cut");
-        jMenu3.add(jMenuItem7);
+        cutTxt.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_K, java.awt.event.InputEvent.CTRL_MASK));
+        cutTxt.setLabel("Cut");
+        jMenu3.add(cutTxt);
 
-        jMenuItem8.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
-        jMenuItem8.setLabel("Select All");
-        jMenu3.add(jMenuItem8);
+        selectTxt.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_A, java.awt.event.InputEvent.CTRL_MASK));
+        selectTxt.setLabel("Select All");
+        jMenu3.add(selectTxt);
 
-        jMenuItem9.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F5, 0));
-        jMenuItem9.setLabel("Time/Date");
-        jMenuItem9.addActionListener(new java.awt.event.ActionListener() {
+        timeandDate.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F5, 0));
+        timeandDate.setLabel("Time/Date");
+        timeandDate.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem9ActionPerformed(evt);
+                timeandDateActionPerformed(evt);
             }
         });
-        jMenu3.add(jMenuItem9);
+        jMenu3.add(timeandDate);
 
         jMenuBar1.add(jMenu3);
 
@@ -141,10 +172,15 @@ public class MainFrame extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(1, 1, 1)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 337, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
@@ -154,14 +190,62 @@ public class MainFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
-    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
+    private void pasteTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pasteTxtActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem6ActionPerformed
+    }//GEN-LAST:event_pasteTxtActionPerformed
 
-    private void jMenuItem9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem9ActionPerformed
+    private void timeandDateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_timeandDateActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem9ActionPerformed
+    }//GEN-LAST:event_timeandDateActionPerformed
 
+    private void searchBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchBtnActionPerformed
+        // TODO add your handling code here:
+        searchArea(jTextArea1, searchField.getText());
+    }//GEN-LAST:event_searchBtnActionPerformed
+    
+    
+    
+    class YellowHighlighter extends DefaultHighlighter.DefaultHighlightPainter{
+        
+        public YellowHighlighter(Color c) {
+            super(c);
+        }
+       
+         
+    }
+    DefaultHighlighter.HighlightPainter highlight = new YellowHighlighter(Color.yellow);
+    
+    public void removeYellow(JTextComponent textComponent){
+        Highlighter removehlight = textComponent.getHighlighter();
+        Highlighter.Highlight[] remove = removehlight.getHighlights();
+        
+        for(int i=0; i<remove.length; i++){
+            if(remove[i].getPainter() instanceof YellowHighlighter){
+            removehlight.removeHighlight(remove[i]);
+        }
+        }
+    }
+    public void searchArea(JTextComponent textComp, String tstring){
+        removeYellow(textComp);
+        try{
+           Highlighter hlight = textComp.getHighlighter();
+           Document doc = textComp.getDocument();
+           String text = doc.getText(0,doc.getLength());
+           
+           int pos = 0;
+           
+           while((pos = text.toUpperCase().indexOf(tstring.toUpperCase(),pos))>=0){
+               hlight.addHighlight(pos, pos+tstring.length(), highlight);
+               pos += tstring.length();
+               
+           }
+           
+        }
+        catch(Exception e){
+           
+            
+        }
+    }
     /**
      * @param args the command line arguments
      */
@@ -198,7 +282,9 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu Search;
+    private javax.swing.JMenuItem copyTxt;
+    private javax.swing.JMenuItem cutTxt;
+    private javax.swing.JMenuItem exitApplication;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
@@ -210,14 +296,14 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem12;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JMenuItem jMenuItem8;
-    private javax.swing.JMenuItem jMenuItem9;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JMenuItem pasteTxt;
+    private javax.swing.JButton searchBtn;
+    private javax.swing.JTextField searchField;
+    private javax.swing.JMenuItem selectTxt;
+    private javax.swing.JMenuItem timeandDate;
     // End of variables declaration//GEN-END:variables
 }
